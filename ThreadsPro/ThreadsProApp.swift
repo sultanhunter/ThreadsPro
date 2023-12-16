dd69ce5a4dd6754141c5bc3586556a745b610ac5
sultan <sultanibneusman0@gmail.com>
2023-12-16 18:03:05 +0530

    Initial Commit

---
+++ ThreadsPro/ThreadsProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsProApp.swift
+//  ThreadsPro
+//
+//  Created by Sultan on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
